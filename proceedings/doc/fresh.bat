7a2e2f92f64ee2bdcbb75f93e33ccf9f5259f610
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2018-11-19 11:27:16 -0800

    Add proceedings skeleton

---
+++ proceedings/doc/fresh.bat
@@ -0,0 +1,16 @@
+# Clean up the directory after a LaTeX build. Windows version
+del Proceedings.aux
+del Proceedings.bbl
+del Proceedings.blg
+del Proceedings.fff
+del Proceedings.lof
+del Proceedings.log
+del Proceedings.lot
+del Proceedings.pdf
+del Proceedings.synctex.gz
+del Proceedings.tex
+del Proceedings.toc
+del Proceedings.ttt
+del Proceedings.upa
+del Proceedings.upb
+del Proceedings-concordance.tex
